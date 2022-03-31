b0a03401c31c536025e4da547ce31b53746bd402
oz-k <cem0614@naver.com>
2022-03-31 21:48:14 +0900

    서버 실행 시 package.json name과 pm2 name이 없으면 에러나게 수정

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,6 @@
+import { ConsoleLogger } from '@nestjs/common';
+import { name as packageName } from 'package.json';
+import { name as pm2Name } from 'pm2-process.json';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AppConfigService } from './config';
@@ -6,6 +9,23 @@ async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     
     const port = app.get(AppConfigService).port || 3000;
-    await app.listen(port, () => console.log(`🔥Server running at port: ${port}`));
+    await app.listen(port, () => {
+        const logger = new ConsoleLogger('NestApplication');
+
+        try {
+            if(!packageName) throw new Error('Package name not set');
+            if(!pm2Name) throw new Error('Pm2 name not set');
+        } catch(err) {
+            logger.error(err.message);
+
+            throw err;
+        }
+
+        if(packageName !== pm2Name) {
+            logger.warn('Package name and Pm2 name are different');
+        }
+        
+        logger.debug(`🔥${packageName} server running at port: ${port}`);
+    });
 }
 bootstrap();
